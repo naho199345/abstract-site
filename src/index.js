4c2b827e97ef03736a401bea60acb3d53efec067
김승호 <naho199345@naver.com>
2022-02-06 18:10:30 +0900

    심화주차 과제

--- src/index.js
+++ src/index.js
@@ -1,10 +1,88 @@
-class Site {}
+class Site {
+    constructor() {
+        this.boards = [];
+    }
 
-class Board {}
+    addBoard(board) {
+        for (let i = 0; i < this.boards.length; i++) {
+            if (this.boards[i].name === board.name) {
+                throw new Error('중복')
+            }
+        }
 
-class Article {}
+        board.isName = true;
+        this.boards.push(board);
+    }
 
-class Comment {}
+    findBoardByName(board) {
+        for (let i = 0; i < this.boards.length; i++) {
+            if (this.boards[i].name === board) {
+                return this.boards[i];
+            }
+        }
+    }
+}
+
+class Board {
+    constructor(name) {
+        if (name === "" || name === null || !name) {
+            throw new Error('no')
+        } else {
+            this.name = name;
+            this.isName = false;
+            this.article = [];
+        }
+    }
+    publish(article) {
+        if (this.isName) {
+            article.id = `${this.name}-${Math.random()}`
+            article.createdDate = `${this.name}-${new Date().toISOString()}`
+            article.iscomment=true;
+            this.article.push(article);
+        } else if (this.isName === false) {
+            throw new Error("no");
+        }
+    }
+    getAllArticles() {
+        return this.article;
+    }
+}
+ 
+class Article {
+    constructor(article) {
+        if (!article.subject || !article.content || !article.author) {
+            throw new Error('no')
+        } else {
+            this.comment = [];
+            this.article=article
+            this.iscomment=false;
+        }
+
+    }
+    reply(comment) {
+        if(this.iscomment){
+            comment.createdDate = `${this.name}-${new Date().toISOString()}`
+            this.comment.push(comment)
+        } else if(this.iscomment===false){
+            throw new Error('no')
+        }
+
+    }
+    getAllComments() {
+        return this.comment
+    }
+}
+
+class Comment {
+    constructor(comment) {
+        if(!comment.content || !comment.author){
+           throw new Error('no')
+        } else{
+            this.comment=comment
+        }
+    }
+}
+
 
 module.exports = {
     Site,
@@ -12,3 +90,20 @@ module.exports = {
     Article,
     Comment,
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
